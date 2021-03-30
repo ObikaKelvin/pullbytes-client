4726f03f0623d3bd829e2ed2199aa1f7574c2daa
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-03-30 18:03:06 +0100

    created a ticket add and edit pages

---
+++ src/views/app-views/admin-views/tickets/add-tickets/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import TicketForm from '../TicketForm';
+
+const AddTicket = () => {
+	return (
+		<TicketForm mode="ADD" />
+	)
+}
+
+export default AddTicket
