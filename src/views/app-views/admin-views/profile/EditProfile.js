e433a05a991e5cb50969bc53f828ae058a3e5024
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-04-17 02:17:56 +0100

    fixed update profile functionality

--- src/views/app-views/admin-views/profile/EditProfile.js
+++ src/views/app-views/admin-views/profile/EditProfile.js
@@ -1,8 +1,9 @@
 import React, {useEffect} from 'react'
-import { useHistory } from 'react-router-dom'
-import { Form, Button, Input, Row, Col, message } from 'antd';
-import UserService from 'services/UserService';
 import { connect } from 'react-redux';
+import { Form, Button, Input, Row, Col, message } from 'antd';
+
+import UserService from 'services/UserService';
+import { setAuthUser } from 'redux/actions/Auth';
 
 const EditProfile = props =>{
 
@@ -11,59 +12,64 @@ const EditProfile = props =>{
 
 	useEffect(() => {
 		form.setFieldsValue({
-			username: user.name,
+			name: user.name,
 			email: user.email})
 	}, [])
 
 	const onFinish = () => {
 		form.validateFields().then(values => {
-			UserService.updatePassword(values).then(( response ) => {
-				message.success({ content: 'Password Changed!', duration: 2 });
+			UserService.updateMe(values).then(( response ) => {
+				props.setAuthUser(response.user);
+				message.success({ content: 'Your profile was updated successfully!', duration: 2 });
 			})
 		});
   	};
 
-	return (
-		<>
-			<h2 className="mb-4">Edit Profile</h2>
-			<Row >
-				<Col xs={24} sm={24} md={24} lg={8}>
-					<Form
-						name="EditProfileForm"
-						layout="vertical"
-						form={form}
-						onFinish={onFinish}
-					>
-						<Form.Item
-							label="User Name"
-							name="username"
-							rules={[{ 
-								required: true,
-								message: 'Please enter username' 
-							}]}
+	if(user){
+		return (
+			<>
+				<h2 className="mb-4">Edit Profile</h2>
+				<Row >
+					<Col xs={24} sm={24} md={24} lg={8}>
+						<Form
+							name="EditProfileForm"
+							layout="vertical"
+							form={form}
+							onFinish={onFinish}
 						>
-							<Input />
-						</Form.Item>
+							<Form.Item
+								label="User Name"
+								name="name"
+								rules={[{ 
+									required: true,
+									message: 'Please enter name' 
+								}]}
+							>
+								<Input />
+							</Form.Item>
+	
+							<Form.Item
+								label="Email"
+								name="email"
+								rules={[{ 
+									required: true,
+									message: 'Please enter email address' 
+								}]}
+							>
+								<Input />
+							</Form.Item>
+	
+							<Button type="primary" htmlType="submit">
+								Update
+							</Button>
+						</Form>
+					</Col>
+				</Row>
+			</>
+		)
+	}
 
-						<Form.Item
-							label="Email"
-							name="email"
-							rules={[{ 
-								required: true,
-								message: 'Please enter email address' 
-							}]}
-						>
-							<Input />
-						</Form.Item>
-
-						<Button type="primary" htmlType="submit">
-							Update
-						</Button>
-					</Form>
-				</Col>
-			</Row>
-		</>
-	)
+	return <></>
 }
 
 const mapStateToProps = ({ auth }) => {
@@ -71,4 +77,6 @@ const mapStateToProps = ({ auth }) => {
 	return { user };
 }
 
-export default connect(mapStateToProps)(EditProfile)
+export default connect(mapStateToProps, {
+	setAuthUser
+})(EditProfile)
